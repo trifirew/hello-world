e638a19589b3726fcaf4c1f23b29aea9978101c2
trifirew <wuqswuqs2001@gmail.com>
2016-10-15 13:11:43 -0400

    Add guess-number.py

---
+++ guess-number.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+print("Guess Number")
+print("By: trifirew")
+
+
+print()
+minn = int(input("What is the minimun number for you to guess? "))
+maxn = int(input("What is the maximum number for you to guess? "))
+print()
+print("You will be guessing a number between " + str(minn) + " and " + str(maxn))
+print("START!")
+rightn = random.randrange(minn, maxn)
+time = 0
+#print(rightn)
+
+while True:
+    print()
+    guessn = int(input("Guess the number: "))
+    time += 1
+    print("You have tried for " + str(time) + " times")
+    if guessn < rightn:
+        print("The number you guess is too SMALL! Try a LARGER number!")
+    elif guessn > rightn:
+        print("The number you guess is too LARGE! Try a SMALLER number!")
+    else:
+        print("YOU ARE RIGHT! THE NUMBER IS " + str(rightn))
+        break
+
+print("Game over")
